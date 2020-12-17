06b948c9d8f95adcaa304a5a5adceaa67c864b23
roxana-torska <roxana.torska@gmail.com>
2020-12-17 14:21:00 +0200

    changed ExpensesListItem component

--- src/components/ExpensesListItem/ExpensesListItem.jsx
+++ src/components/ExpensesListItem/ExpensesListItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ExpensesListItem = () => {
-  return (
-    <li className="expenses-list-item list-group-item d-flex align-items-center">
-      <span className="expenses-list-item__date pr-3">2017-04-25</span>
-      <span className="expenses-list-item__title px-3 flex-grow-1">Jogurt</span>
-      <span className="expenses-list-item__value pl-3">2 USD</span>
+const ExpensesListItem = ({ expense }) => {
+    const { date, value, currency, title } = expense;
+  
+    return (
+      <li className="expenses-list-item list-group-item d-flex align-items-center">
+      <span className="expenses-list-item__date pr-3">{date}</span>
+      <span className="expenses-list-item__title px-3 flex-grow-1">{title}</span>
+      <span className="expenses-list-item__value pl-3">{value} {currency}</span>
     </li>
   );
 };
